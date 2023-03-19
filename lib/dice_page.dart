ac3eb3da333617a072e1f17dc301252896d35622
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-19 20:22:44 +0900

    dice_page

--- lib/dice_page.dart
+++ lib/dice_page.dart
@@ -11,8 +11,46 @@ class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Dice'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(35),
+              child: Row(
+                children: [
+                  Expanded(
+                    // // flex - 요소들의 공간 크기 배치 지정
+                    // flex: 2,
+                    child: Image.asset(
+                      'images/dice1.png',
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Image.asset(
+                      'images/dice2.png',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 60),
+            ButtonTheme(
+                minWidth: 100,
+                height: 60,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.play_circle_fill,
+                      size: 60,
+                    )))
+          ],
+        ),
       ),
     );
   }
