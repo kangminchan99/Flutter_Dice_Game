8977793ad25a1b2df259f133fc294f689d731d3e
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-19 20:23:08 +0900

    GestureDetector(keyboard_ontap)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -28,76 +28,84 @@ class _LoginPageState extends State<LoginPage> {
       ),
       // snack bar에 context를 전달해주기 위해 SingleChildScrollView를 빌더 위젯으로 감싸준다.
       body: Builder(builder: (context) {
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              const Center(
-                child: Image(
-                  image: AssetImage(
-                    'images/yeji.jpeg',
+        // GestureDetector - 사용자의 클릭을 감지
+        return GestureDetector(
+          // 텍스트 필드에서 정보 입력 후 화면의 빈공간을 클릭할 시 키보드가 사라진다.
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Center(
+                  child: Image(
+                    image: AssetImage(
+                      'images/yeji.jpeg',
+                    ),
+                    width: 270,
                   ),
-                  width: 270,
                 ),
-              ),
-              // TextFeild로 정보를 구현하는 화면을 구성할 때  사용
-              Form(
-                  child: Theme(
-                      data: ThemeData(
-                          // 텍스트필드 클릭 시 굵은 teal형태로 강조
-                          primaryColor: Colors.teal,
-                          inputDecorationTheme: const InputDecorationTheme(
-                              labelStyle:
-                                  TextStyle(color: Colors.teal, fontSize: 15))),
-                      child: Container(
-                        padding: const EdgeInsets.all(30),
-                        child: Column(children: [
-                          TextField(
-                            controller: idController,
-                            decoration: const InputDecoration(
-                                labelText: 'Enter "yeji"'),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextField(
-                            controller: pwController,
-                            decoration: const InputDecoration(
-                                labelText: 'Enter Password'),
-                            keyboardType: TextInputType.text,
-                            // obscureText: true - 패스워드는 보안이 필요하므로 문자가 표시 되지 않게 해준다.
-                            obscureText: true,
-                          ),
-                          const SizedBox(height: 15),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              IconButton(
-                                  onPressed: () {
-                                    if (idController.text == 'yeji' &&
-                                        pwController.text == '1234') {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (BuildContext context) =>
-                                                  const DicePage()));
-                                    } else if (idController.text == 'yeji' &&
-                                        pwController.text != '1234') {
-                                      showSnackBar2(context);
-                                    } else if (idController.text != 'yeji' &&
-                                        pwController.text == '1234') {
-                                      showSnackBar3(context);
-                                    } else {
-                                      showSnackBar(context);
-                                    }
-                                  },
-                                  icon: const Icon(
-                                    Icons.arrow_forward,
-                                    color: Colors.black,
-                                    size: 30,
-                                  )),
-                            ],
-                          )
-                        ]),
-                      )))
-            ],
+                // TextFeild로 정보를 구현하는 화면을 구성할 때  사용
+                Form(
+                    child: Theme(
+                        data: ThemeData(
+                            // 텍스트필드 클릭 시 굵은 teal형태로 강조
+                            primaryColor: Colors.teal,
+                            inputDecorationTheme: const InputDecorationTheme(
+                                labelStyle: TextStyle(
+                                    color: Colors.teal, fontSize: 15))),
+                        child: Container(
+                          padding: const EdgeInsets.all(30),
+                          child: Column(children: [
+                            TextField(
+                              controller: idController,
+                              decoration: const InputDecoration(
+                                  labelText: 'Enter "yeji"'),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              controller: pwController,
+                              decoration: const InputDecoration(
+                                  labelText: 'Enter Password'),
+                              keyboardType: TextInputType.text,
+                              // obscureText: true - 패스워드는 보안이 필요하므로 문자가 표시 되지 않게 해준다.
+                              obscureText: true,
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      if (idController.text == 'yeji' &&
+                                          pwController.text == '1234') {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder:
+                                                    (BuildContext context) =>
+                                                        const DicePage()));
+                                      } else if (idController.text == 'yeji' &&
+                                          pwController.text != '1234') {
+                                        showSnackBar2(context);
+                                      } else if (idController.text != 'yeji' &&
+                                          pwController.text == '1234') {
+                                        showSnackBar3(context);
+                                      } else {
+                                        showSnackBar(context);
+                                      }
+                                    },
+                                    icon: const Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.black,
+                                      size: 30,
+                                    )),
+                              ],
+                            )
+                          ]),
+                        )))
+              ],
+            ),
           ),
         );
       }),
